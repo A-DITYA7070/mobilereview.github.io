6f49e1e309533af4f15bb2eea8b1583fbff2a1da
A-DITYA7070 <99244575+A-DITYA7070@users.noreply.github.com>
2022-05-26 23:13:58 +0530

    mobile-revviewing-web

---
+++ mail.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "adityaraj993148@gmail.com";
+$subject = "Mail From Foxfoster";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@Foxfoster" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+header("Location:thankyou.html");
+?>
